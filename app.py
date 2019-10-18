a3e3ab56f0ee334eb19e65c7e43145de99afb423
George Aoyagi <aoyagigeorge@gmail.com>
2019-10-18 09:42:25 -0700

    added play again feature

--- app.py
+++ app.py
@@ -118,7 +118,6 @@ def exchange(deckID, cardsLeft, position, cardCode):
     emptyDiscard = False
     position = int(position)
     print(position)
-    print(hand)
     changed = hand[position]          #gets the card that will be replaced
     requests.get(f"https://deckofcardsapi.com/api/deck/{deckID}/pile/hand/add/?cards={cardCode}")               #adds new card to the pile
     requests.get(f"https://deckofcardsapi.com/api/deck/{deckID}/pile/hand/draw/?cards={changed['code']}")       #draws the card to remove from the hand pile
